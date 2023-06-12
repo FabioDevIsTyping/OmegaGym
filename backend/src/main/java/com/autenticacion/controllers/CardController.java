dcdad6fe5dd6bede8df53cce61ed1cc89a64e1df
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-06-12 14:50:11 +0200

    changed imports

--- backend/src/main/java/com/autenticacion/controllers/CardController.java
+++ backend/src/main/java/com/autenticacion/controllers/CardController.java
@@ -4,14 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.autenticacion.models.Card;
 import com.autenticacion.repositories.CardRepository;
--- backend/src/main/java/com/autenticacion/controllers/SubscriptionController.java
+++ backend/src/main/java/com/autenticacion/controllers/SubscriptionController.java
@@ -4,14 +4,7 @@ package com.autenticacion.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.autenticacion.models.Subscription;
 import com.autenticacion.repositories.SubscriptionRepository;
