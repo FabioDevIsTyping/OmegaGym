d02902db74231251500706ebdc0a2dcd79b37429
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-06-18 17:56:23 +0200

    deleted stuff

--- frontendweb/src/app/app.module.ts
+++ frontendweb/src/app/app.module.ts
@@ -2,19 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddUserComponent } from './pages/add-user/add-user.component';
-import { UserListComponent } from './pages/user-list/user-list.component';
-import { RolesComponent } from './pages/roles/roles.component';
-import { EmployeeComponent } from './pages/employee/employee.component';
 import { LoginComponent } from './pages/login/login.component';
-import { UserLayoutComponent } from './pages/user-layout/user-layout.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeLayoutComponent } from './pages/home-layout/home-layout.component';
-import { VoidTableComponent } from './reusable/void-table/void-table.component';
-import { TabsComponent } from './reusable/tabs/tabs.component';
-import { QuizAppComponent } from './pages/quiz-app/quiz-app.component';
-import { StepperCrudComponent } from './pages/stepper-crud.component/stepper-crud.component';
 import { CardComponent } from './pages/card/card.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
@@ -26,17 +16,7 @@ import { AddSubscriptionComponent } from './pages/add-subscription/add-subscript
 @NgModule({
   declarations: [
     AppComponent,
-    AddUserComponent,
-    UserListComponent,
-    RolesComponent,
-    EmployeeComponent,
     LoginComponent,
-    UserLayoutComponent,
-    HomeLayoutComponent,
-    VoidTableComponent,
-    TabsComponent,
-    QuizAppComponent,
-    StepperCrudComponent,
     CardComponent,
     NavbarComponent,
     AdminPageComponent,
